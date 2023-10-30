46ff52aeddc4dae74eaea5832ae259e50bce107e
Dumilonze <elkevndrwlt@gmail.com>
2023-10-30 10:31:17 +0200

    Add file reading errors

--- scorer.py
+++ scorer.py
@@ -1,8 +1,16 @@
 import sys
 
+INPUT_DIRECTORY = "input_data"
 def main(args):
-    print(args[1])
-
+    try:
+        file_path = f"{INPUT_DIRECTORY}/{args[1]}"
+        try:
+            with open(file_path, 'r') as file:
+                str_data = file.read()
+        except FileNotFoundError:
+            print(f"Error: File not found. Please check the file path. (..{file_path})")
+    except IndexError:
+        print("Error: Provide file name as srgument when running script")
 
 
 if __name__ == "__main__":
